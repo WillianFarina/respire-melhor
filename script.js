281f6e71b865df551a41d37de111f0ac1eea1a2f
WillianFarina <61472577+WillianFarina@users.noreply.github.com>
2020-06-17 00:05:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const container = document.querySelector('.container');
+const text = document.querySelector('#text');
+const start = document.querySelector('.top button');
+const pointer = document.querySelector('.pointer-container');
+
+const totalTime = 7500
+const breatheTime = (totalTime / 5) * 2
+const holdTime = totalTime / 5
+
+start.addEventListener('click', () => {
+  if(pointer.classList.contains('active')) {
+    pointer.classList.remove('active')
+    start.innerHTML = 'Iniciar'
+    text.innerHTML = 'Relaxe!'
+  } else {
+    start.innerHTML = 'Parar'
+    pointer.classList.add('active')
+    breatheAnimation()
+  }
+})
+
+function breatheAnimation() {
+  if (pointer.classList.contains('active')) {
+    text.innerHTML = 'Inspire!'
+    container.className = 'container grow'
+
+    setTimeout(() => {
+      text.innerText = 'Segure'
+
+      setTimeout(() => {
+        text.innerText = 'Expire!'
+        container.className = 'container shrink'
+      }, holdTime)
+
+    }, breatheTime)
+
+    setInterval(breatheAnimation, totalTime)
+  }
+  
+}
